c7b3a49015cbf8fe79ddfcde24e05a4e4b1d72fe
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-02-16 10:44:00 +0100

    add portfolio projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -34,14 +34,20 @@ const Portfolio = () => {
                                 <img 
                                 src={port.cover}
                                 className="portfolio-image"
-                                alt="portfolio" />
+                                alt="portfolio-project" />
                                 <div className="content">
                                     <p className="title">{port.name}</p>
                                     <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
+                                    <div>
+                                        <button
+                                            className="btn"
+                                            onClick={() => window.open(port.url)}
+                                        >Website</button>
+                                        <button
+                                        className="btn btn2"
+                                        onClick={() => window.open(port.linkGit)}
+                                        >Git</button>
+                                    </div>
                                 </div>
                             </div>
                         )
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,32 +1,23 @@
 export const portfolioData = [
     {
-        cover: "/portfolio/1/one.png",
-        title: "GGW",
-        description: "React, JSX, Tailwind, Jest",
-        url: "http://google.com"
+        cover: "/portfolio/1/Yariga.png",
+        name: "Yriga - A Real Estate App",
+        description: "MERN - MongoDB, Express, React, Node.js - A work in progress",
+        url: "https://refine-dashboard-max.netlify.app",
+        linkGit: "https://github.com/MaxiStahl1992/refine_dashboard"
     },
     {
-        cover: "/portfolio/1/one.png",
-        title: "GGWP",
-        description: "React, JSX, Tailwind, Jest",
-        "url": "http://google.com"
+        cover: "/portfolio/1/Allianz.png",
+        name: "Allianz Project",
+        description: "Angular, Spring Boot (Kotlin), PostgreSQL",
+        url: "",
+        linkGit: ""
     },
     {
-        cover: "/portfolio/1/one.png",
-        title: "GGWP",
-        description: "React, JSX, Tailwind, Jest",
-        url: "http://google.com"
+        cover: "/portfolio/1/AlinaPortfolio.png",
+        name: "Alinas Portfolio",
+        description: "React",
+        url: "https://alinabuck.github.io/",
+        linkGit: "https://github.com/MaxiStahl1992/react_portfolio_alina"
     },
-    {
-        cover: "/portfolio/1/one.png",
-        title: "GGWP",
-        description: "React, JSX, Tailwind, Jest",
-        url: "http://google.com"
-    },
-    {
-        cover: "/portfolio/1/one.png",
-        title: "GGWP",
-        description: "React, JSX, Tailwind, Jest",
-        url: "http://google.com"
-    }
 ]
